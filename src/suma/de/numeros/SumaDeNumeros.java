824f3e86ef3916dbef7fc680946abc44fb1f86d4
Iamfrans <1323544@senati.pe>
2023-03-17 11:11:32 -0500

    Scanner

--- src/suma/de/numeros/SumaDeNumeros.java
+++ src/suma/de/numeros/SumaDeNumeros.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package suma.de.numeros;
-
+import java.util.Scanner;
 /**
  *
  * @author Alumno
@@ -14,7 +14,11 @@ public class SumaDeNumeros {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner (System.in);
+        //impleamos la entada de texto
+        
+        System.out.println("ingrese su nombre: ");
+        String nombre = leer.next();
     }
     
 }
