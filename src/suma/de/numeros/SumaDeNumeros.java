74cdf73c8f63c6a4040ea57a938a2047eed76c6e
Iamfrans <1323544@senati.pe>
2023-03-17 11:09:10 -0500

    Creando algoritmo

---
+++ src/suma/de/numeros/SumaDeNumeros.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma.de.numeros;
+
+/**
+ *
+ * @author Alumno
+ */
+public class SumaDeNumeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
